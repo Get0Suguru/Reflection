97a3797728918d64b0753c09b29b84322fb4c590
Gautam Taneja <gautam45taneja@gmail.com>
2025-06-05 21:50:11 +0530

    added typed exceptions to keep controller responses cleaner. more polish later

---
+++ src/main/java/com/geto/suguru/reflection/exception/JournalException.java
@@ -0,0 +1,7 @@
+package com.geto.suguru.reflection.exception;
+
+public class JournalException extends Exception{
+    public JournalException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/geto/suguru/reflection/exception/SecurityChainException.java
@@ -0,0 +1,7 @@
+package com.geto.suguru.reflection.exception;
+
+public class SecurityChainException extends Exception{
+    public SecurityChainException(String message) {
+        super(message);
+    }
+}
