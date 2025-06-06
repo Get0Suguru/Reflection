46ce9defc4b0f399b759932d4b87c108e8a1767d
Gautam Taneja <gautam45taneja@gmail.com>
2025-06-06 22:05:27 +0530

    dropping a JournalDto placeholder for future mapping—parking it till we need projections

---
+++ src/main/java/com/geto/suguru/reflection/payload/JournalDto.java
@@ -0,0 +1,12 @@
+package com.geto.suguru.reflection.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+public class JournalDto {
+
+
+}
