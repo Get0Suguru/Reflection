9f7bfc4a1bce91111249ddd2fba9dc7ac43fc2c7
Gautam Taneja <gautam45taneja@gmail.com>
2025-08-04 07:21:53 +0530

    trying junit ran first test

---
+++ src/test/java/com/geto/suguru/reflection/service/UserServiceTests.java
@@ -0,0 +1,13 @@
+package com.geto.suguru.reflection.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserServiceTests {
+
+    @Test
+    public void firstTest(){
+        assertEquals(4, 2+1);
+    }
+}
