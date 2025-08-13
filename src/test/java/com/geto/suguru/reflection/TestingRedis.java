4102505f763155a10590b0b5858014dcbc3a8a8d
Gautam Taneja <gautam45taneja@gmail.com>
2025-08-13 10:08:58 +0530

    included redis

--- src/test/java/com/geto/suguru/reflection/TestingRedis.java
+++ src/test/java/com/geto/suguru/reflection/TestingRedis.java
@@ -1,4 +1,23 @@
 package com.geto.suguru.reflection;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
 public class TestingRedis {
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    @Disabled
+    @Test
+    public void testRedis(){
+        redisTemplate.opsForValue().set("papa", "bhosda");
+        Object val = redisTemplate.opsForValue().get("papa");
+        System.out.println(val);
+        System.out.println(redisTemplate.opsForValue().get("randi"));
+    }
 }
